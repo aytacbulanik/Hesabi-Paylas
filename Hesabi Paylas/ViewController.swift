18c6776ade3d08897ee5b2ed650bcb6f9f9fb88c
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-09-23 22:30:22 +0300

    all components are connected

--- Hesabi Paylas/ViewController.swift
+++ Hesabi Paylas/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tenButtonOut: UIButton!
+    
+    @IBOutlet weak var thirtyButtonOut: UIButton!
+    @IBOutlet weak var twentyButtonOut: UIButton!
+    @IBOutlet weak var costField: UITextField!
+    
+    @IBOutlet weak var stepperOut: UIStepper!
+    @IBOutlet weak var personLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func stepperPressed(_ sender: UIStepper) {
+        print(sender.value)
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        
+    }
 }
 

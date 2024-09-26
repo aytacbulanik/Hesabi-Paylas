4599dfc7e78d153f65febb7fb63ffa6cda12d385
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-09-27 00:24:56 +0300

    resultVC desinged

--- Hesabi Paylas/Controller/ResultVC.swift
+++ Hesabi Paylas/Controller/ResultVC.swift
@@ -9,15 +9,28 @@ import UIKit
 
 class ResultVC: UIViewController {
     
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
     var result : CostResult?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         guard let result else { return }
-        let result = (doubleCost + (doubleCost * tip/100)) / person
+        let doubleCost = Double(result.cost)
+        let tip = Double(result.tip)
+        let person = Double(result.person)
+        let sonuc = (doubleCost + (doubleCost * tip/100)) / person
+        
     }
     
 
     
-
+    @IBAction func reCalculateButton(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+    
 }
